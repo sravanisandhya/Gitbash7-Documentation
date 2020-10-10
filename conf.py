1ba18d84e08a1f00a2b8d670a2984eed766b2ab6
sravanisandhya <72546806+sravanisandhya@users.noreply.github.com>
2020-10-09 23:02:16 -1200

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Android Aplication Development'
+copyright = '2020, Sandhya'
+author = 'Sandhya'
 
 # The full version, including alpha/beta/rc tags
 release = ''
